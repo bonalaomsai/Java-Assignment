407ff27ac0ed48ed2f24ff07bcd208f172fceeb2
bonalaomsai <134691502+bonalaomsai@users.noreply.github.com>
2023-06-10 19:31:33 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,34 @@
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // Normalize k if it is greater than n
+
+        // Reverse the entire array
+        reverse(nums, 0, n - 1);
+        // Reverse the first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse the remaining n-k elements
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotate(nums, k);
+
+        System.out.print("Rotated Array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
