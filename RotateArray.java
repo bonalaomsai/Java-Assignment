0f93e5e6db9a1fac5da041f13571266374be1f46
bonalaomsai <134691502+bonalaomsai@users.noreply.github.com>
2023-06-10 19:32:19 +0530

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,3 +1,4 @@
+/*Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.*/
 public class RotateArray {
     public static void rotate(int[] nums, int k) {
         int n = nums.length;
