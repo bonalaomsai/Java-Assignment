716650273421eb1d6bed1e19be7e254f1016eae6
bonalaomsai <134691502+bonalaomsai@users.noreply.github.com>
2023-06-10 11:42:30 +0530

    Update LudoGame.java

--- LudoGame.java
+++ LudoGame.java
@@ -1,1 +1,3 @@
+/*Chef is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a 6 on the die.
 
+In the current turn, Chef rolled the numberX on the die. Determine if Chef can enter a new token into the play in the current turn or not.*/
