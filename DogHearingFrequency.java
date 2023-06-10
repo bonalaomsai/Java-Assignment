ffb788f725dd5cd7d35781a0692b450f04058a86
bonalaomsai <134691502+bonalaomsai@users.noreply.github.com>
2023-06-10 15:22:59 +0530

    Update DogHearingFrequency.java

--- DogHearingFrequency.java
+++ DogHearingFrequency.java
@@ -1,1 +1,27 @@
+/*Chef's dog binary hears frequencies starting from 67 Hertz to 45000 Hertz (both inclusive).
+If Chef's commands have a frequency of X Hertz, find whether binary can hear them or not.*/
+import java.util.Scanner;
 
+public class DogHearing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the number of test cases
+        int T = scanner.nextInt();
+
+        // Iterate through each test case
+        for (int i = 0; i < T; i++) {
+            // Read the frequency of Chef's command
+            int frequency = scanner.nextInt();
+
+            // Check if the frequency is within the audible range
+            if (frequency >= 67 && frequency <= 45000) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        scanner.close();
+    }
+}
