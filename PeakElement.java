b6bed5e48aea9ad5237f7ddaef42e70a9511d645
bonalaomsai <134691502+bonalaomsai@users.noreply.github.com>
2023-06-10 19:15:43 +0530

    Update PeakElement.java

--- PeakElement.java
+++ PeakElement.java
@@ -1,1 +1,30 @@
+/*A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.*/
+public class FindPeakElement {
+    public static int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                // Move towards the increasing side
+                left = mid + 1;
+            } else {
+                // Move towards the decreasing side
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int peakIndex = findPeakElement(nums);
+        System.out.println("Peak element index: " + peakIndex);
+    }
+}
 
